ee7e2c0be4e807455f2094a425464ba38f39ffdb
mamjerez@gmail.com <mamjerez@gmail.com>
2023-06-25 06:37:38 +0200

    Comprueba si existen codigos nuevos

--- gastos.js
+++ gastos.js
@@ -57,6 +57,13 @@ function excelToJson(filePath) {
   // Convierte la hoja de cálculo a JSON
   let jsonData = XLSX.utils.sheet_to_json(sheet);
 
+    // Define el array para almacenar los elementos nuevos
+    let newCapitulos = [];
+    let newOrganicos = [];
+    let newProgramas = [];
+    let newEconomicos = [];
+
+
   // Elimina las columnas excluidas
   jsonData = jsonData.map((row) => {
     columnsToExclude.forEach((column) => {
@@ -64,6 +71,7 @@ function excelToJson(filePath) {
     });
 
     const newRow = {};
+  
     Object.keys(row).forEach((key) => {
       // Renombra las columnas
       const newKey = keyMapping[key] || key;
@@ -81,26 +89,79 @@ function excelToJson(filePath) {
       if (newRow.hasOwnProperty('CodEco')) {
         newRow['CodCap'] = parseInt(newRow['CodEco'].toString().charAt(0), 10);
 
-         // Añade descripcion de capítulos
-         const capitulo = gastosEconomicaCapitulos.find((cap) => cap.codigo === newRow['CodCap']);
-         newRow['DesCap'] = capitulo ? capitulo.descripcion : '';
+        // Añade descripcion de capítulos
+        const capitulo = gastosEconomicaCapitulos.find((cap) => cap.codigo === newRow['CodCap']);
+        if (capitulo) {
+          newRow['DesCap'] = capitulo.descripcion;
+        } else {
+          newRow['DesCap'] = '';
+          newCapitulos.push(newRow['CodCap']);  
+        }
 
         // Añade descripcion de organicos
         const organico = gastosOrganicaOrganicos.find((org) => org.codigo === newRow['CodOrg']);
-        newRow['DesOrg'] = organico ? organico.descripcion : '';
+        if (organico) {
+          newRow['DesOrg'] = organico.descripcion;
+        } else {
+          newRow['DesOrg'] = '';
+          newOrganicos.push(newRow['CodOrg']);  
+        }
 
-         // Añade descripcion de programas
-         const programa = gastosProgramaProgramas.find((pro) => pro.codigo === newRow['CodPro']);
-         newRow['DesPro'] = programa ? programa.descripcion : '';
+        // Añade descripcion de programas
+        const programa = gastosProgramaProgramas.find((pro) => pro.codigo === newRow['CodPro']);
+        if (programa) {
+          newRow['DesPro'] = programa.descripcion;
+        } else {
+          newRow['DesPro'] = '';
+          newProgramas.push(newRow['CodPro']);  
+        }
 
         // Añade descripcion de económicos
         const economico = gastosEconomicaEconomicos.find((eco) => eco.CodEco === newRow['CodEco']);
-        newRow['DesEco'] = economico ? economico.DesEco : '';
+        if (economico) {
+          newRow['DesEco'] = economico.descripcion;
+        } else {
+          newRow['DesEco'] = '';
+          newEconomicos.push(newRow['CodEco']);  
+        }
+
       }
     });
 
-    return newRow;
+       return newRow;
   });
+
+    // Imprime capitulos nuevos
+let newCapitulosUnicos = [...new Set(newCapitulos)];
+if (newCapitulosUnicos.length > 0)  {
+  console.log("Capitulos nuevos: ", newCapitulosUnicos);
+  pathNewCapitulos = pathExcel + 'newCapitulos.json';
+  fs.writeFileSync(pathNewCapitulos, JSON.stringify(newCapitulosUnicos, null, 2));
+}
+
+    // Imprime organicos nuevos
+let newOrganicosUnicos = [...new Set(newOrganicos)];
+if (newOrganicosUnicos.length > 0)  {
+  console.log("Organicos nuevos: ", newOrganicosUnicos);
+  pathNewOrganicos = pathExcel + 'newOrganicos.json';
+  fs.writeFileSync(pathNewOrganicos, JSON.stringify(newOrganicosUnicos, null, 2));
+}
+
+  // Imprime programas nuevos
+let newProgramasUnicos = [...new Set(newProgramas)];
+if (newProgramasUnicos.length > 0)  {
+  console.log("Programas nuevos: ", newProgramasUnicos);
+  pathNewProgramas = pathExcel + 'newProgramas.json';
+  fs.writeFileSync(pathNewProgramas, JSON.stringify(newProgramasUnicos, null, 2));
+}
+
+  // Imprime economicos nuevos
+  let newEconomicosUnicos = [...new Set(newEconomicos)];
+  if (newEconomicosUnicos.length > 0)  {
+    console.log("Económicos nuevos: ", newEconomicosUnicos);
+    pathNewEconomicos = pathExcel + 'newEconomicos.json';
+    fs.writeFileSync(pathNewEconomicos, JSON.stringify(newEconomicosUnicos, null, 2));
+  }
 
   jsonData.shift(); // Remueve el primer objeto (títulos de las columnas)
   jsonData.pop(); // Remueve el último objeto (totales)
@@ -108,8 +169,11 @@ function excelToJson(filePath) {
   return jsonData;
 }
 
+ 
+
 // Guarda los datos en formato JSON en un nuevo archivo
 pathJson = pathExcel + '2023LiqGas.json';
+pathProgramasNuevos = pathExcel + 'newProgramasUnicos.json';
 
 // Si el archivo existe, borra el archivo existente
 fs.unlink(pathJson, (err) => {
