0f0836b6167fa061ab1e5862bbbb3d7e0dd86953
mamjerez@gmail.com <mamjerez@gmail.com>
2024-09-26 10:15:39 +0200

    Refactor file paths and update JSON file names for 2024 budget

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/2024.08.27/',
-    nameExcelIngresos: 'Estado_Ejecucion_Ingresos_2024_por_aplicaciones_a_27-08-2024.xls',
-    nameExcelGastos: 'Estado_Ejecucion_Gastos_2024_por_aplicaciones_a_27-08-2024.xls',
+    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/2024.09.25/',
+    nameExcelIngresos: 'Estado_Ejecucion_Ingresos_2024_por_aplicaciones_a_25-09-2024.xls',
+    nameExcelGastos: 'Estado_Ejecucion_Gastos_2024_por_aplicaciones_a_25-09-2024.xls',
     year: '2024',
     pathDataJsonNecesarios: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/Tablas/JsonNecesariosApp/',
     pathApp: 'D:/presupuestos/src/assets/data/',
