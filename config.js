a397b571b239674945ad719d009cbecf55af57b2
mamjerez@gmail.com <mamjerez@gmail.com>
2025-06-17 11:52:56 +0200

    Update Excel file paths and names for June 2025

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2025/Ejecucion/2025.05.12/',
+    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2025/Ejecucion/2025.06.12/',
     //    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/pruebas/',
     pathApp: 'D:/ocm/src/assets/data/',
     // pathApp: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/pruebasApp/',
     pathDataJsonNecesarios: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/Tablas/JsonNecesariosApp/',
 
-    nameExcelIngresos: 'Estado_Ejecucion_Ingresos_2025_por_aplicaciones_a_12-05-2025.xls',
-    nameExcelGastos: 'Estado_Ejecucion_Gastos_2025_por_aplicaciones_a_12-05-2025.xls',
+    nameExcelIngresos: 'Estado_Ejecucion_Ingresos_2025_por_aplicaciones_a_12-06-2025.xlsx',
+    nameExcelGastos: 'Estado_Ejecucion_Gastos_2025_por_aplicaciones_a_12-06-2025.xlsx',
     year: '2025',
 };
