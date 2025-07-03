8566f796b84cca357bc68ba674a00f05fd380c69
mamjerez@gmail.com <mamjerez@gmail.com>
2025-07-03 10:58:21 +0200

    Update configuration for 2024; adjust Excel file paths and names

--- config.js
+++ config.js
@@ -1,11 +1,34 @@
+const year = '2024';
+const month = '12';
+const day = '31';
+
+const dateFolder = `${year}.${month}.${day}`;
+const dateFile = `${day}-${month}-${year}`;
+
 module.exports = {
-    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2025/Ejecucion/2025.06.12/',
-    //    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/pruebas/',
+    // Exporta también month, day, dateFolder y dateFile por si se necesitan en otros módulos.
+    year,
+    month,
+    day,
+    dateFolder,
+    dateFile,
+    pathExcel: `C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/${year}/Ejecucion/${dateFolder}/`,
     pathApp: 'D:/ocm/src/assets/data/',
-    // pathApp: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/pruebasApp/',
     pathDataJsonNecesarios: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/Tablas/JsonNecesariosApp/',
 
-    nameExcelIngresos: 'Estado_Ejecucion_Ingresos_2025_por_aplicaciones_a_12-06-2025.xlsx',
-    nameExcelGastos: 'Estado_Ejecucion_Gastos_2025_por_aplicaciones_a_12-06-2025.xlsx',
-    year: '2025',
+    nameExcelIngresos: `Estado_Ejecucion_Ingresos_${year}_por_aplicaciones_a_${dateFile}.xlsx`,
+    nameExcelGastos: `Estado_Ejecucion_Gastos_${year}_por_aplicaciones_a_${dateFile}.xlsx`,
 };
+
+
+// module.exports = {
+//     year: '2024',
+//     pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/2024.12.31/',
+//     //    pathExcel: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/pruebas/',
+//     pathApp: 'D:/ocm/src/assets/data/',
+//     // pathApp: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/2024/Ejecucion/pruebasApp/',
+//     pathDataJsonNecesarios: 'C:/Users/Usuario/OneDrive/Ayuntamiento/Presupuestos/Tablas/JsonNecesariosApp/',
+
+//     nameExcelIngresos: 'Estado_Ejecucion_Ingresos_2024_por_aplicaciones_a_31-12-2024.xlsx',
+//     nameExcelGastos: 'Estado_Ejecucion_Gastos_2024_por_aplicaciones_a_31-12-2024.xlsx',
+// };
